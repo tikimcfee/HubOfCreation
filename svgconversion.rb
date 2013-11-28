11a19b601d8e7f03d590b59c6048f7bd33a61f63
Ivan Lugo <ivanlugo@Ivans-MacBook-Air.local>
2013-11-27 19:59:20 -0500

    Will now automatically create and storage images in their own directory. This is done with a series of change directories, which will be used until a newer parameter is found that allows for rmagick to write to a specific directory.
    	/destination/(file_name)/

--- svgconversion.rb
+++ svgconversion.rb
@@ -6,8 +6,8 @@ include Magick
 absolutePath = File.dirname(File.expand_path('__FILE__'))
 
 # Locate source folder, cd to it
-sourceDir = File.join(absolutePath, "source")
-Dir.chdir(sourceDir)
+sourceDir = File.join(absolutePath, "source/")
+#Dir.chdir(sourceDir)
 
 # Create list of svg files to convert, then strip to filename only
 # This needs to be done with a regular expression, for sure
@@ -19,9 +19,12 @@ end
 
 # load up the files
 svg_hash = {}
-svgNames.each do |fileName|
-	if fileName.include? ".svg"
-		svg_hash.merge!( { (fileName[0.. -5] + ".png") => Magick::ImageList.new(fileName)} )
+svgNames.each do |file_name|
+	if file_name.include? ".svg"
+		name_of_file = file_name[0 .. -5]
+				#name_of_file.gsub!(/\s+/, "") if name_of_file.include? " "
+		FileUtils.mkdir_p("destination/" + name_of_file )
+		svg_hash.merge!( { (name_of_file + ".png") => Magick::ImageList.new(sourceDir + file_name) } )
 	end
 end
 
@@ -37,19 +40,19 @@ svg_hash.each do |file_name, img|
 	svg_hash[file_name] = img.paint_transparent('white', opacity=TransparentOpacity, invert=false, fuzz=27500)
 end
 
-# test method
-# svgTransparent = svgSources.collect { |svg| svg.opacity = 127 unless svg }
 
 # check for and make a new directory for conversions, then move to it
 # TODO
 # ! -- does Dir.chdir need to use absolute pathing for different localities?
 # 	-- even if it does, get used to passing absolute pathing, could cause fewer problems
-Dir.chdir(absolutePath)
-FileUtils.mkdir("destination") unless File.directory?("destination")
-Dir.chdir("destination")
+
+#Dir.chdir(absolutePath)
+#FileUtils.mkdir("destination") unless File.directory?("destination")
+#Dir.chdir("destination")
 
 
 # write images
 svg_hash.each do |file_name, img|
+	Dir.chdir(absolutePath + "/destination/" + file_name[0.. -5] + "/")
 	img.write(file_name)
 end
